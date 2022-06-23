b004350bb25c4879bbfe6b54d8d29bf204db0c08
Rmx1921 <koreanplay14340@gmail.com>
2022-06-23 12:08:37 +0400

    trying to increase piwerlevels

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -296,7 +296,6 @@ static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src_sdm670[] = {
 	F(650000000, P_CRC_DIV,  1, 0, 0),
 	F(700000000, P_CRC_DIV,  1, 0, 0),
 	F(750000000, P_CRC_DIV,  1, 0, 0),
-	F(780000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -655,10 +654,10 @@ static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL] =
 		565000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
-		650000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 750000000;
+		610000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 650000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		780000000;
+		750000000;
 }
 
 static int gpu_cc_gfx_sdm845_fixup(struct platform_device *pdev)
